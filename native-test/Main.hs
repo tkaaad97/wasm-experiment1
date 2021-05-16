3ce359b9aa71bd3fb3240af8f9ff177a09681b4d
tkaaad97 <tkaaad97@gmail.com>
2021-05-16 17:59:23 +0900

    implementing native test

---
+++ native-test/Main.hs
@@ -0,0 +1,95 @@
+module Main where
+
+import Data.Bits ((.&.))
+import qualified Data.ByteString as ByteString (readFile)
+import Data.Word (Word32, Word8)
+import Foreign (Ptr)
+import qualified Foreign
+import SampleLang.Compile (compileWat)
+import System.Process (callCommand)
+import Wasmtime
+import Wasmtime.Raw as Wasmtime
+
+foreign import ccall "printf" printf :: Ptr () -> IO ()
+foreign import ccall "printf" printfI :: Ptr () -> Word32 -> IO ()
+foreign import ccall "printf" printfSI :: Ptr () -> Ptr () -> Word32 -> IO ()
+foreign import ccall "printf" printfSII :: Ptr () -> Ptr () -> Word32 -> Word32 -> IO ()
+
+printf' :: Ptr (Ptr Word8) -> Ptr WasmValVecT -> Ptr WasmValVecT -> IO (Ptr WasmTrapT)
+printf' memPtr paramVecPtr _ = do
+    WasmValVecT _ p <- Foreign.peek paramVecPtr
+    WasmValI64 offLen <- Foreign.peek p
+    let off = offLen .&. 0xFFFF
+    mem <- Foreign.peek memPtr
+    printf (mem `Foreign.plusPtr` fromIntegral off)
+    return Foreign.nullPtr
+
+printfI' :: Ptr (Ptr Word8) -> Ptr WasmValVecT -> Ptr WasmValVecT -> IO (Ptr WasmTrapT)
+printfI' memPtr paramVecPtr _ = do
+    WasmValVecT _ p <- Foreign.peek paramVecPtr
+    WasmValI64 offLen <- Foreign.peek p
+    WasmValI32 a  <- Foreign.peekElemOff p 1
+    let off = offLen .&. 0xFFFF
+    mem <- Foreign.peek memPtr
+    printfI (mem `Foreign.plusPtr` fromIntegral off) a
+    return Foreign.nullPtr
+
+printfSI' :: Ptr (Ptr Word8) -> Ptr WasmValVecT -> Ptr WasmValVecT -> IO (Ptr WasmTrapT)
+printfSI' memPtr paramVecPtr _ = do
+    WasmValVecT _ p <- Foreign.peek paramVecPtr
+    WasmValI64 offLen <- Foreign.peek p
+    WasmValI64 offLen2 <- Foreign.peekElemOff p 1
+    WasmValI32 a  <- Foreign.peekElemOff p 2
+    let off = offLen .&. 0xFFFF
+        off2 = offLen2 .&. 0xFFFF
+    mem <- Foreign.peek memPtr
+    printfSI (mem `Foreign.plusPtr` fromIntegral off) (mem `Foreign.plusPtr` fromIntegral off2) a
+    return Foreign.nullPtr
+
+printfSII' :: Ptr (Ptr Word8) -> Ptr WasmValVecT -> Ptr WasmValVecT -> IO (Ptr WasmTrapT)
+printfSII' memPtr paramVecPtr _ = do
+    WasmValVecT _ p <- Foreign.peek paramVecPtr
+    WasmValI64 offLen <- Foreign.peek p
+    WasmValI64 offLen2 <- Foreign.peekElemOff p 1
+    WasmValI32 a  <- Foreign.peekElemOff p 2
+    WasmValI32 b  <- Foreign.peekElemOff p 3
+    let off = offLen .&. 0xFFFF
+        off2 = offLen2 .&. 0xFFFF
+    mem <- Foreign.peek memPtr
+    printfSII (mem `Foreign.plusPtr` fromIntegral off) (mem `Foreign.plusPtr` fromIntegral off2) a b
+    return Foreign.nullPtr
+
+printWasmtimeError :: Foreign.Ptr WasmtimeErrorT -> IO ()
+printWasmtimeError = (putStrLn =<<) . getWasmtimeErrorMessage
+
+main :: IO ()
+main =
+    withWasmEngine $ \engine ->
+    withWasmStore engine $ \store -> do
+        callCommand "gcc -E -P -o native-test/code/test1.i native-test/code/test1.c"
+        compileWat "native-test/code/test1.i" "native-test/code/test1.wat"
+        wasmSource <- wat2Wasm =<< ByteString.readFile "native-test/code/test1.wat"
+        withWasmtimeModule engine wasmSource printWasmtimeError $ \module_ ->
+            Foreign.alloca $ \memPtr ->
+            withWasmtimeInstance store module_ (callbacks memPtr) printWasmtimeError $ \instance_ ->
+                withWasmInstanceExports instance_ $ \exports -> do
+                    mem <- getExportMemory exports 0
+                    memData <- wasmMemoryData mem
+                    Foreign.poke memPtr memData
+                    func <- getExportFunc exports 1
+                    withWasmtimeFuncCall func [] printWasmtimeError (const $ putStrLn "trap") $ \results ->
+                        putStrLn $ "results: " ++ show results
+    where
+    getExportFunc xs idx
+        | length xs > idx = wasmExternAsFunc (xs !! idx)
+        | otherwise = error "export index out of bounds"
+    getExportMemory xs idx
+        | length xs > idx = wasmExternAsMemory (xs !! idx)
+        | otherwise = error "export index out of bounds"
+
+    callbacks memPtr =
+        [ (FuncType [wasmValKindI64] [], printf' memPtr)
+        , (FuncType [wasmValKindI64, wasmValKindI32] [], printfI' memPtr)
+        , (FuncType [wasmValKindI64, wasmValKindI64, wasmValKindI32] [], printfSI' memPtr)
+        , (FuncType [wasmValKindI64, wasmValKindI64, wasmValKindI32, wasmValKindI32] [], printfSII' memPtr)
+        ]
---
+++ native-test/code/test1.c
@@ -0,0 +1,43 @@
+#define WITH_STR(expr) expr, #expr
+
+void printf(string s);
+void printf_i(string s, int a);
+void printf_s_i(string s, string a, int b);
+void printf_s_i_i(string s, string a, int b, int c);
+
+int failure = 0;
+
+void foo() { printf("OK\n"); }
+
+int add(int a, int b) { return a + b; }
+
+bool assert_int(int expected, int actual, string expr) {
+    if (actual == expected) {
+        printf_s_i("OK %s => %d\n", expr, actual);
+        return true;
+    } else {
+        printf_s_i_i("NG %s => %d expected, but got %d\n", expr, expected, actual);
+        failure = failure + 1;
+        return false;
+    }
+}
+
+int main() {
+    printf("start test\n");
+    assert_int(0, WITH_STR(0));
+    assert_int(42, WITH_STR(42));
+    assert_int(21, WITH_STR(5+20-4));
+    assert_int(47, WITH_STR(5+6*7));
+    assert_int(15, WITH_STR(5*(9-6)));
+    assert_int(4, WITH_STR((3+5)/2));
+    assert_int(10, WITH_STR(-10+20));
+    assert_int(1, WITH_STR((-1-2)/-3));
+
+    if (failure == 0) {
+        printf("all test passed.");
+    } else {
+        printf_i("%d tests failed.\n", failure);
+    }
+
+    return failure;
+}
